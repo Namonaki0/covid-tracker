7d0ddd32b16325d63be27dc50c8a3348b3774b31
Andre <andreferreira@live.co.uk>
2023-01-22 23:54:55 +0000

    data fetch and rendering

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="text-center bg-slate-600 text-white p-4 mb-10">
+  <header class="text-center bg-slate-600 text-white p-4 mb-10">
     <p class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-viruses"></i> Covid-19 tracker</p>
     <span>API by <a class="text-blue-200" href="https://covid19api.com" target="_blank">covid19api.com</a></span>
-  </div>
+  </header>
 </template>
 
 <script setup>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,50 @@
+<template>
+    <main v-if="!loading">
+        {{ dataObj.countries }}
+    </main>
+
+    <main class="flex flex-col align-center justify-center text-center" v-else>
+        <div class="text-gray-500 text-3xl mt-10 mb-6">
+            fetching data...
+        </div>
+        <img :src="loadingImage" class="w-24 m-auto" alt="">
+    </main>
+</template>
+
 <script setup>
 
-</script>
+import { onMounted, ref } from "vue";
 
-<template>
-</template>
+const props = defineProps({
+    loading: {
+        type: Boolean,
+        default: true,
+    },
+    title: {
+        type: String,
+        default: 'Global'
+    },
+    loadingImage: require('../assets/hourglass.gif')
+})
+
+const dataObj = ref({
+    date: '',
+    stats: {},
+    countries: [],
+    loading: true
+})
+
+onMounted(async () => {
+    const res = await fetch('https://api.covid19api.com/summary');
+    const data = await res.json();
+    console.log(data)
+
+    const dataObj = {
+        date: data.Date,
+        stats: data.Global,
+        countries: data.Countries,
+        loading: false
+    }
+})
+
+</script>
